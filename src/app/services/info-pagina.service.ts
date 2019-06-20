1a6d289dc7ce48a8136ecfdfe807a22c102e3a86
Alejandro <alejandrocruz.x819@gmail.com>
2019-06-19 23:58:02 -0500

    seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,14 +9,25 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient ) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo() {
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
     } );
+  }
 
+  private cargarEquipo() {
+    this.http.get('https://portafolio-ce1fe.firebaseio.com/equipo')
+    .subscribe( (resp: any) => {
+      this.equipo = resp;
+    } );
   }
 }
